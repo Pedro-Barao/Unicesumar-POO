8231bfff72a657eec3120033205a68229fc06446
Pedro Barão <predrobarao183@gmail.com>
2026-08-04 15:13:17 -0300

    Create Main.java

---
+++ Exercicio 01/Main.java
@@ -0,0 +1,18 @@
+public class Main
+  {
+
+    public static void main(String[] args)
+    {
+      Carro meuCarro = new Carro();
+      meuCarro.modelo = "Fusca";
+      meuCarro.cor = "Azul";
+
+      meuCarro.buzinar();
+
+      Carro meuCarro2 = new Carro();
+      meuCarro2.modelo = "Camaro";
+      meuCarro2.cor = "Amarelo";
+
+      meuCarro2.buzinar();
+    }
+  }
