0ca6e461dc5f9270ff41c46d0f3f413ddeeda0c6
Pedro Barão <predrobarao183@gmail.com>
2026-08-04 14:59:34 -0300

    Create Apresentacao.java

---
+++ Exercício 00/Apresentacao.java
@@ -0,0 +1,7 @@
+public class Apresentacao
+  {
+    public static void main(String[] args)
+    {
+      System.out.println("Olá, meu nome é Pedro Gonella Barão! Missão 00 concluida com sucesso");
+    }
+  }
